312eb2219bfe72a70ad2e45e74250132c8dc1736
Amiri Prescod <presso1717@gmail.com>
2026-07-16 15:34:53 -0400

    Add edit and delete routes

--- tracker/urls.py
+++ tracker/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("", views.job_list, name="job_list"),
     path("jobs/add/", views.job_create, name="job_create"),
     path("jobs/<int:job_id>/", views.job_detail, name="job_detail"),
+    path("jobs/<int:job_id>/edit/", views.job_edit, name="job_edit"),
+    path("jobs/<int:job_id>/delete/", views.job_delete, name="job_delete"),
 ]
