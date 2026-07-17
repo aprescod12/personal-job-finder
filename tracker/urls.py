5cdd2475aea9ca39d251b6bf79828f74abf3cc18
Amiri Prescod <presso1717@gmail.com>
2026-07-17 05:04:50 -0400

    Route listing verification workflow

--- tracker/urls.py
+++ tracker/urls.py
@@ -15,6 +15,11 @@ urlpatterns = [
     ),
     path("jobs/add/", views.job_create, name="job_create"),
     path("jobs/<int:job_id>/", views.job_detail, name="job_detail"),
+    path(
+        "jobs/<int:job_id>/verify/",
+        views.job_listing_verify,
+        name="job_listing_verify",
+    ),
     path("jobs/<int:job_id>/match/", views.job_match, name="job_match"),
     path(
         "jobs/<int:job_id>/requirements/",
