fea705d453f6b0d7bc2b726a61dbd2da01cf005c
Amiri Prescod <presso1717@gmail.com>
2026-07-21 20:24:46 -0400

    Configure intake history app

---
+++ intake_history/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class IntakeHistoryConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "intake_history"
+    verbose_name = "Job intake history"
