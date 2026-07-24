bd0bb189293cb7d8d7e9fc587f226ad8f9c226d1
Amiri Prescod <presso1717@gmail.com>
2026-07-24 14:18:17 -0400

    Use certifi trust bundle for discovery HTTPS

--- job_discovery/apps.py
+++ job_discovery/apps.py
@@ -1,3 +1,6 @@
+import os
+
+import certifi
 from django.apps import AppConfig
 
 
@@ -5,3 +8,9 @@ class JobDiscoveryConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "job_discovery"
     verbose_name = "Job Discovery"
+
+    def ready(self):
+        # Python installations on macOS do not always expose a usable default CA
+        # path to OpenSSL. Point standard-library HTTPS clients at the project's
+        # maintained certifi bundle while preserving any deliberate user override.
+        os.environ.setdefault("SSL_CERT_FILE", certifi.where())
